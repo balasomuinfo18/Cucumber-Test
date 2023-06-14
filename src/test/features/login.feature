158b676c5e89eaa287d8ce2f55a86866f32dccfb
balasomuinfo18 <balasomuinfo18@gmail.com>
2023-06-14 20:17:20 +0530

    Test cases updated
    Test cases updated

--- src/test/features/login.feature
+++ src/test/features/login.feature
@@ -3,10 +3,60 @@ Feature: Login Action
 Scenario: Successful Login with Valid Credentials
 
 	Given User is on Login Page
-	When User Navigate to LogIn Page
+	Given User enters UserName 
+	Given User enters Password 
+	When user click on the login button
+	Then Message displayed Login Successfully
+
+Scenario:  Login with invalid username and valid password
+
+	Given User is on Login Page
+	Given User enters invalid username 
+	Given User enters valid password 
+	When user click on the login button
+    Then Message displayed Invalid UserName or Password
+
+Scenario:  Login with valid username and invalid password
+	Given User is on Login Page
+	Given User enters valid username 
+	Given User enters invalid password
+	When user click on the login button
+    Then Message displayed Invalid UserName or Password
+	
+Scenario:  Login with invalid username and invalid password	
+	
+	Given User is on Login Page
+	Given User enters invalid username 
+	Given User enters invalid password
+	When user click on the login button
+    Then Message displayed Invalid UserName or Password
+
+Scenario: signin with Facebook	
+    Given User is on Login Page
+    Given User signup with Facebook
+	When User Navigate to Facebook signin Page
 	And User enters UserName and Password
 	Then Message displayed Login Successfully
 
-Scenario: Successful LogOut
-	When User LogOut from the Application
-	Then Message displayed LogOut Successfully
+Scenario: signin with Google	
+    Given User is on Login Page
+    Given User signup with Google
+	When User Navigate to Google signin Page
+	And User enters UserName and Password
+	Then Message displayed Login Successfully
+
+Scenario:Stay signin Functionality
+  Given User is on Login Page
+  Given User click the Stay Signed In checkbox
+
+Scenario:Stay signin Functionality
+Given User is on Login Page
+
+Scenario:Verify Broken links on the home page
+Given User is on Login Page
+
+Scenario:Forgot Password
+Given User is on Login Page
+
+Scenario:Terms & Conditions
+Given User is on Login Page
--- src/test/steps/loginSteps.js
+++ src/test/steps/loginSteps.js
@@ -1,16 +1,95 @@
 const { Given,When,Then } = require('@cucumber/cucumber');
-const { test, expect } = require('@playwright/test');
+const {page, browser, chromium,} = require('@playwright/test');
 
- Given('User is on Login Page', async function () {
-    await page.goto('https://www.testyou.in/Login.aspx');
-    await expect(page).toHaveTitle(/Hi! Visitor You CanLoginOrCreate An Account/);
-  });
 
- When('User Navigate to LogIn Page', async function () {
-    await page.goto('https://www.testyou.in/Login.aspx');
-    await expect(page).toHaveTitle(/Hi! Visitor You CanLoginOrCreate An Account/);
-  });
-  Then('User enters UserName and Password', async function () {
-    await page.goto('https://www.testyou.in/Login.aspx');
-    await expect(page).toHaveTitle(/Hi! Visitor You CanLoginOrCreate An Account/);
-  });
+         Given('User is on Login Page', async function () {
+           browser = await chromium.launch({headless: false});
+           page = await browser.newPage();
+           await page.goto("https://www.testyou.in/Login.aspx")
+         });
+
+         Given('User enters UserName', async function (username) {
+          await page.locator("(//span[text()='Email Address / Login Id: ']/following::input)[1]").type(username)
+        });
+        
+       
+        Given('User enters Password', async function (password) {
+            await page.locator("(//input[@type='password'])[2]").type(password)
+
+         });
+
+         When('user click on the login button', async function () {
+            await page.locator("(//input[@value='Login'])[2]").click
+         });
+
+
+         Then('Message displayed Login Successfully', async function () {
+          await page.goto("https://www.testyou.in/Login.aspx")});
+
+
+         Given('User is on Login Page', async function () {
+           browser = await chromium.launch({headless: false});
+           page = await browser.newPage();
+           await page.goto("https://www.testyou.in/Login.aspx")
+         });
+
+         Given('User enters invalid username', async function (invaliduser) {
+          await page.locator("(//span[text()='Email Address / Login Id: ']/following::input)[1]").type(invaliduser)
+        });
+        
+       
+        Given('User enters Password', async function (password) {
+            await page.locator("(//input[@type='password'])[2]").type(password)
+
+         });
+
+         When('user click on the login button', async function () {
+            await page.locator("(//input[@value='Login'])[2]").click
+         });
+
+
+         Then('Message displayed Invalid UserName or Password', async function () {
+          await page.goto("https://www.testyou.in/Login.aspx")});
+
+
+          Given('User is on Login Page', async function () {
+            browser = await chromium.launch({headless: false});
+            page = await browser.newPage();
+            await page.goto("https://www.testyou.in/Login.aspx")
+          });
+ 
+          Given('User enters UserName', async function (username) {
+           await page.locator("(//span[text()='Email Address / Login Id: ']/following::input)[1]").type(username)
+         });
+         
+        
+         Given('User enters invalid password', async function (invalidpassword) {
+             await page.locator("(//input[@type='password'])[2]").type(invalidpassword)
+ 
+          });
+ 
+          When('user click on the login button', async function () {
+             await page.locator("(//input[@value='Login'])[2]").click
+          });
+ 
+ 
+          Then('Message displayed Invalid UserName or Password', async function () {
+           await page.goto("https://www.testyou.in/Login.aspx")});
+
+          Given('User enters invalid UserName', async function (invalidusername) {
+           await page.locator("(//span[text()='Email Address / Login Id: ']/following::input)[1]").type(invalidusername)
+         });
+         
+        
+         Given('User enters invalid password', async function (invalidpassword) {
+             await page.locator("(//input[@type='password'])[2]").type(invalidpassword)
+ 
+          });
+ 
+          When('user click on the login button', async function () {
+             await page.locator("(//input[@value='Login'])[2]").click
+          });
+ 
+ 
+          Then('Message displayed Invalid UserName or Password', async function () {
+           await page.goto("https://www.testyou.in/Login.aspx")});
